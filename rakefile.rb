9d728e1c3b0a76049d75f9f1231c42e7637e8a4c
Joe Crotty <josephcrotty@gmail.com>
2011-12-10 12:23:18 -0700

    add rakefile

---
+++ rakefile.rb
@@ -0,0 +1,39 @@
+require 'rake/clean'
+require 'Find'
+
+WORKINGDIR = './**'
+
+CLEAN.add('./dist/')
+dist_exclude = [ './',
+                './build',
+                'build.xml',
+                './dist',
+                './offline'
+              ]
+
+task :dist => :clean do
+    FileUtils.mkdir('dist')
+    fileList = FileList.new(WORKINGDIR).exclude(*dist_exclude)
+    #dirList = FileList.new(WORKINGDIR + '/**/').exclude('./', './dist/', './offline/')
+    #dirList = FileList.new(WORKINGDIR + '/**/').exclude(*dist_exclude)
+    #pathList = Find.find(WORKINGDIR)
+    #fileList = FileList.new(WORKINGDIR + '/**')
+    fileList.each do |file|
+        #if File.directory?(path)
+            #puts "path - #{path}"
+            #next
+            #dist_exclude.each do |pattern|
+                #puts "pattern - #{pattern}"
+                #if path.match(/#{pattern}/i)
+                    puts file 
+                #end
+            #end
+        #end
+    end
+    #fileList.each do |file|
+            #FileUtils.cp_r dir, './dist', :verbose => true, :preserve => true
+        #if ! File.exists?(file)
+            #puts file
+        #end
+    #end
+end
