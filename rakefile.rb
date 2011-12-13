1da8eb2cc2ce81ad9322978b8da1e056b5c9aada
Joe Crotty <josephcrotty@gmail.com>
2011-12-12 20:15:03 -0700

    more changes

--- rakefile.rb
+++ rakefile.rb
@@ -1,35 +1,47 @@
+# Standard use is to be run from Jenkins
+# If you need to run locally set following Jenkins environment variables:
+# export WORKSPACE='/home/USER/git/foo'
+
 require 'rake/clean'
 
-# for local testing only
-#ENV['WORKSPACE'] = '/Users/heathercrotty/git/sandbox'
+DIST = ENV['WORKSPACE'] + 'dist/'
+LOGS = ENV['WORKSPACE'] + 'build/logs'
+STAGE_HOST = 'stage.tst.returnpath.net'
+STAGE_CI_HOST = 'stage-ci.returnpath.net'
+
+CLEAN.add(ENV['WORKSPACE'] + '/dist/')
+CLEAN.add(ENV['WORKSPACE'] + '/build/')
 
 dist_exclude = [ 
                  '**/build.xml',
                  '**/build',
                  '**/dist',
+                 '**/hudsonBuild.properties',
+                 '**/logs',
                  '**/offline',
                  '**/rakefile.rb',
                  '**/test'
                ]
 
+#def rsync(arg1, arg2)
+#end
+
+for env in ENV do
+  puts env
+end
+
 # tasks
 task :default => [:build]
 
-CLEAN.add(ENV['WORKSPACE'] + '/dist/')
-CLEAN.add(ENV['WORKSPACE'] + '/build/')
-
-desc '[build]'
 task :build => [:clean, :init, :dist] do
 end
 
-desc '[init]'
 task :init do
   FileUtils.mkdir_p( ENV['WORKSPACE'] + '/build/logs')
+  FileUtils.mkdir(ENV['WORKSPACE'] + '/dist')
 end
 
-desc '[dist]'
 task :dist do
-  FileUtils.mkdir(ENV['WORKSPACE'] + '/dist')
   fileList = FileList.new(ENV['WORKSPACE'] + '/**').exclude(*dist_exclude)
   fileList.each do |file|
     #puts file
