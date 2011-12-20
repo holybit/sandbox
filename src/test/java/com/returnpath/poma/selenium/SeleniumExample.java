896153aa5e84f273cff60802730dbd3ccda98e3e
Joe Crotty <josephcrotty@gmail.com>
2011-12-19 20:40:17 -0700

    more changes

--- src/test/java/com/returnpath/poma/selenium/SeleniumExample.java
+++ src/test/java/com/returnpath/poma/selenium/SeleniumExample.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
+@Test
 public class SeleniumExample {
     public static void main(String args[]) throws Exception {
         URL server = new URL("http://localhost:4444/wd/hub/");
