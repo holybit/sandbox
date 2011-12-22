7f8bca515aa0f1828c79e727e87a753a93fefd00
Joe Crotty <joe.crotty@returnpath.net>
2011-12-22 13:25:31 -0700

    rake rsync/deploy WIP
    Change-Id: Ia6ff6c534f87f809ac95231fe64ec2614d20ba5b

--- rakefile.rb
+++ rakefile.rb
@@ -1,16 +1,24 @@
 # Standard use is to be run from Jenkins
-# If you need to run locally set following Jenkins environment variables:
-# export WORKSPACE='/home/USER/git/foo'
+# If you want to run non Jenkins test builds set shell variable below
+# export RP_BUILD_TEST=true
 
-#require 'open4'
+require 'open4'
 require 'rake/clean'
 
+if ! ENV['RP_BUILD_TEST'].nil?
+  puts "RP_BUILD_TEST=" + ENV['RP_BUILD_TEST']
+  ENV['WORKSPACE'] = File.dirname(__FILE__)
+elsif ENV['WORKSPACE'].nil?
+  puts 'can not find shell environment variable WORKSPACE - exit'
+  exit 1
+end
+puts "WORKSPACE=" + File.dirname(__FILE__)
+
 # constants
+DATETIME = Time.now.strftime("%Y%m%d%H%M%S")
 DIST = ENV['WORKSPACE'] + '/dist'
 BUILD = ENV['WORKSPACE'] + '/build'
 LOGS = ENV['WORKSPACE'] + '/build/logs'
-STAGE_HOST = 'stage.tst.returnpath.net'
-STAGE_CI_HOST = 'stage-ci.returnpath.net'
 
 CLEAN.add(DIST)
 CLEAN.add(BUILD)
@@ -21,11 +29,49 @@ dist = [
         'README',
        ]
 
-def deploy
+def deploy(src='dist/', user='cmuser', host='test', dest)
+  if host != 'ci' && host != 'test'
+    puts 'deploy HOST must be either ci or test - exit'
+    puts host
+    exit 1
+  elsif dest.nil?
+    puts 'deploy DEST not supplied - exit'
+    exit 1
+  end
+
+  stagedir = '/usr/local/stage'
+
+  if ! ENV['RP_BUILD_TEST'].nil?
+    dest = '/tmp'
+    rhost = 'localhost'
+    user = ENV['USER']
+  elsif host == 'ci'
+    dest = '/usr/local/stage/test/' + dest
+    rhost = 'stage.tst.returnpath.net'
+  elsif host == 'test'
+    dest = '/usr/local/stage/ci_test/' + dest
+    rhost = 'stage-ci.tst.returnpath.net'
+  end
+
+  rargs = [ 
+           '-av',
+           '--backup',
+           "--backup-dir=backup/push_" + DATETIME,
+           '--delete',
+           '--compress',
+           '--checksum',
+           '--perms',
+           '--size-only',
+           '--stats'
+          ]
+
+  rsync = '/usr/bin/rsync ' + rargs.join(' ') + ' ' + src + ' ' + user + '@' + rhost + ':' + dest + '/'
+  #puts rsync                        
+    
   status =
     Open4::popen4("sh") do |pid, stdin, stdout, stderr|
-      stdin.puts "echo 42.out"
-      stdin.puts "echo 42.err 1>&2"
+      stdin.puts rsync
+      #stdin.puts "echo 42.err 1>&2"
       stdin.close
 
       puts "pid        : #{ pid }"
@@ -39,7 +85,7 @@ end
 # tasks
 task :default => [:build]
 
-task :build => [:clean, :init, :dist] do
+task :build => [:clean, :init, :dist, :deploy] do
 end
 
 task :init do
@@ -50,11 +96,11 @@ end
 task :dist do
   fileList = Dir[*dist]
   fileList.each do |file|
-    puts file
+    #puts file
     FileUtils.cp_r file, DIST, :preserve => true
   end
 end
 
 task :deploy do
-
+  deploy('dist/', 'cmuser', 'test', 'www/fbl')
 end
