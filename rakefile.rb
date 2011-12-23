fbe973e4e9033810c34cdc72682397d5dacdcfea
Joe Crotty <josephcrotty@gmail.com>
2011-12-22 20:04:25 -0700

    more rsync work

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,11 @@
-# Standard use is to be run from Jenkins
-# If you want to run non Jenkins test builds set shell variable below
+# Standard use is to be run from Jenkins where the WORKSPACE and other
+# environment variables are picked up from.
+#
+# Note, if you want to run non Jenkins test builds:
 # export RP_BUILD_TEST=true
+# The following will then apply:
+#   1. deploy task will rsync dist to /tmp/rsync_ci or /tmp/rsync_test
+#   2. 
 
 require 'open4'
 require 'rake/clean'
@@ -16,22 +21,25 @@ puts "WORKSPACE=" + File.dirname(__FILE__)
 
 # constants
 DATETIME = Time.now.strftime("%Y%m%d%H%M%S")
-DIST = ENV['WORKSPACE'] + '/dist'
-BUILD = ENV['WORKSPACE'] + '/build'
-LOGS = ENV['WORKSPACE'] + '/build/logs'
-
-CLEAN.add(DIST)
-CLEAN.add(BUILD)
-
+dirs = {
+        'log' => 'build/log',
+        'logSelnim' => 'build/log/selenium'
+       }
 dist = [ 
         'documentroot',
         'includes',
-        'README',
+        'README'
        ]
+puts defined? alldirs
+
+CLEAN.add('dist/')
+CLEAN.add('build/')
 
 def deploy(src='dist/', user='cmuser', host='test', dest)
+  puts 'here joe'
+  puts defined? alldirs
   stagedir = '/usr/local/stage'
-  rsync_test = '/tmp/rsync_test'
+  rsyncLocal = '/tmp/stage'
 
   if host != 'ci' && host != 'test'
     puts 'deploy HOST must be either ci or test - exit'
@@ -42,52 +50,60 @@ def deploy(src='dist/', user='cmuser', host='test', dest)
     exit 1
   end
 
-  if ! ENV['RP_BUILD_TEST'].nil?
-    dest = '/tmp/rsync_test'
-  elsif host == 'ci'
-    dest = '/usr/local/stage/test/' + dest
-    rhost = 'stage.tst.returnpath.net'
-  elsif host == 'test'
-    dest = '/usr/local/stage/ci_test/' + dest
-    rhost = 'stage-ci.tst.returnpath.net'
-  end
+  #if ! ENV['RP_BUILD_TEST'].nil?
+    #if host == 'ci'
+      #dest = rsyncLocal + '/ci_test'
+      #puts dirs.fetch('log')
+      ##log12 = dirs.fetch('log') + '/rsync_ci.log'
+    #else
+      #dest = rsyncLocal + '/test'
+      ##log12 = dirs.fetch('log') + '/rsync_test.log'
+    #end
+  #elsif host == 'ci'
+    #dest = '/usr/local/stage/test/' + dest
+    ##log = "#{dirs['log']}" + '/rsync_ci.log'
+    #rhost = 'stage.tst.returnpath.net'
+  #elsif host == 'test'
+    #dest = '/usr/local/stage/ci_test/' + dest
+    ##log = "#{dirs['log']}" + '/rsync_test.log'
+    #rhost = 'stage-ci.tst.returnpath.net'
+  #end
 
-  rargs = [ 
-           '-av',
-           '--backup',
-           "--backup-dir=backup/push_" + DATETIME,
-           '--delete',
-           '--compress',
-           '--checksum',
-           '--perms',
-           '--size-only',
-           '--stats'
-          ]
+  #rargs = [ 
+           #'-av',
+           #'--backup',
+           #'--backup-dir=backup/push_' + DATETIME,
+           #'--delete',
+           #'--compress',
+           #'--checksum',
+           #'--perms',
+           #'--size-only',
+           #'--stats'
+          #]
 
-  if ! ENV['RP_BUILD_TEST'].nil?
-    if File.directory? rsync_test
-      FileUtils.rm_rf(rsync_test)
-      FileUtils.mkdir(rsync_test)
-    end
-    rsync = '/usr/bin/rsync ' + rargs.join(' ') + ' ' + src + ' ' + dest + '/'
-  else
-    rsync = '/usr/bin/rsync ' + rargs.join(' ') + ' ' + src + ' ' + user + '@' + rhost + ':' + dest + '/'
-  end
-  puts rsync
-  exit
+  #if ! ENV['RP_BUILD_TEST'].nil?
+    #if File.directory? rsyncLocal
+      #FileUtils.rm_rf(rsyncLocal)
+    #end
+      #FileUtils.mkdir(rsyncLocal)
+    #rsync = '/usr/bin/rsync ' + rargs.join(' ') + ' ' + src + ' ' + dest + '/' + ' > ' + log + ' 2>&1'
+  #else
+    #rsync = '/usr/bin/rsync ' + rargs.join(' ') + ' ' + src + ' ' + user + '@' + rhost + ':' + dest + '/'
+  #end
+  #puts rsync
     
-  status =
-    Open4::popen4("sh") do |pid, stdin, stdout, stderr|
-      stdin.puts rsync
-      #stdin.puts "echo 42.err 1>&2"
-      stdin.close
+  #status =
+    #Open4::popen4("sh") do |pid, stdin, stdout, stderr|
+      #stdin.puts rsync
+      ##stdin.puts "echo 42.err 1>&2"
+      #stdin.close
 
-      puts "pid        : #{ pid }"
-      puts "stdout     : #{ stdout.read.strip }"
-      puts "stderr     : #{ stderr.read.strip }"
-    end
-      puts "status     : #{ status.inspect }"
-      puts "exitstatus : #{ status.exitstatus }"
+      #puts "pid        : #{ pid }"
+      #puts "stdout     : #{ stdout.read.strip }"
+      #puts "stderr     : #{ stderr.read.strip }"
+    #end
+      #puts "status     : #{ status.inspect }"
+      #puts "exitstatus : #{ status.exitstatus }"
 end
 
 # tasks
@@ -97,15 +113,15 @@ task :build => [:clean, :init, :dist, :deploy] do
 end
 
 task :init do
-  FileUtils.mkdir_p(LOGS)
-  FileUtils.mkdir(DIST)
+  FileUtils.mkdir_p(alldirs['log'])
+  FileUtils.mkdir('dist/')
 end
 
 task :dist do
   fileList = Dir[*dist]
   fileList.each do |file|
     #puts file
-    FileUtils.cp_r file, DIST, :preserve => true
+    FileUtils.cp_r file, 'dist/', :preserve => true
   end
 end
 
