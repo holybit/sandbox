bb26ac8287300881508a3cac505cb6aa34690fde
Joe Crotty <joe.crotty@returnpath.net>
2012-03-29 15:27:59 -0600

    update version
    Change-Id: Iace0ba9133fbf620193d1eaea1de3282e9a7f92a

--- test/selenium/selenium_start.bash
+++ test/selenium/selenium_start.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-java -jar /Users/heathercrotty/git/sandbox/lib/selenium-server-standalone-2.15.0.jar -role hub > sl_hub_server.log 2>&1 &
+java -jar ../../lib/selenium-server-standalone-2.20.0.jar -role hub > sl_hub_server.log 2>&1 &
 sleep 15
-java -jar /Users/heathercrotty/git/sandbox/lib/selenium-server-standalone-2.15.0.jar -role webdriver -nodeConfig config.json -hub http://127.0.0.1:4444/grid/register > slNodes.log 2>&1 &
+java -jar ../../lib/selenium-server-standalone-2.20.0.jar -role webdriver -nodeConfig config.json -hub http://127.0.0.1:4444/grid/register > slNodes.log 2>&1 &
